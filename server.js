8efea41b1d78b613291431e19502a656b8f62159
Enrique Garcia <garcia.enrique.torres@gmail.com>
2019-05-07 16:26:01 -0500

    package

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tecxam'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/tecxam/index.html'));
-});
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/tecxam/index.html'));
+  });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
